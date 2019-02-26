a7d80ded52902f31f9ae3772863d1dfd4585e5ac
Brian G <brian.gregorius@gmail.com>
2019-02-26 17:22:57 -0500

    completed insert into bst and updated queue so size would be a counter

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,14 +1,21 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        if value < self.value and self.left:
+            self.left.insert(value)
+        elif value < self.value:
+            self.left = BinarySearchTree(value)
+        elif value > self.value and self.right:
+            self.right.insert(value)
+        elif value > self.value:
+            self.right = BinarySearchTree(value)
 
-  def contains(self, target):
-    pass
+    def contains(self, target):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass
